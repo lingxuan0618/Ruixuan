fdfc31085cb327b47256e893795b8cadd7f0ecdb
lingxuan <lingxuan0618@gmail.com>
2022-05-17 21:58:23 +0800

    edit md to sm

---
+++ js/swiper.js
@@ -0,0 +1,23 @@
+const swiperAbout = new Swiper('.swiperAbout', {
+  loop:true,
+  spaceBetween: 50,
+  slidesPerView: 1,
+  autoplay:true,
+  autoplay: {
+    delay: 5000,
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    bulletClass: 'swiper-pagination-bullet',
+    clickable: true,
+  },
+});
+const swiperInterest = new Swiper('.swiperInterest', {
+  loop:true,
+  spaceBetween: 50,
+  slidesPerView: 1,
+  autoplay:true,
+  autoplay: {
+    delay: 5000,
+  },
+});
---
+++ js/type.js
@@ -0,0 +1,10 @@
+const typed = new Typed('#typed', {
+  stringsElement: '#typed-strings',
+  typeSpeed: 70,
+  startDelay: 60,
+  backSpeed: 50,
+  backDelay: 500,
+  loop: true,
+  loopCount: false,
+  showCursor: false,
+});
