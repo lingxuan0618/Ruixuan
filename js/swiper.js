f9d18747d1b3d7835b8258f32e1efffbfca4670d
lingxuan <lingxuan0618@gmail.com>
2022-05-27 22:35:28 +0800

    修改輪播點擊圓點不會繼續輪播之問題/調整Menu的字體大小

--- js/swiper.js
+++ js/swiper.js
@@ -2,9 +2,10 @@ const swiperAbout = new Swiper('.swiperAbout', {
   loop:true,
   spaceBetween: 50,
   slidesPerView: 1,
-  autoplay:true,
+  // autoplay: true,
   autoplay: {
-    delay: 5000,
+    delay: 3000,
+    disableOnInteraction: false,
   },
   pagination: {
     el: '.swiper-pagination',
@@ -16,7 +17,7 @@ const swiperInterest = new Swiper('.swiperInterest', {
   loop:true,
   spaceBetween: 50,
   slidesPerView: 1,
-  autoplay:true,
+  autoplay: true,
   autoplay: {
     delay: 5000,
   },
